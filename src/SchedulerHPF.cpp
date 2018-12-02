76a5bb0be3d00dea51217c8f73b28eefb8cde3ea
SayedAlesawy <Sayed.Kotb97@eng-st.cu.edu.eg>
2018-12-02 16:43:24 +0200

    Fixed context switching problem in HPF

--- src/SchedulerHPF.cpp
+++ src/SchedulerHPF.cpp
@@ -1,22 +1,20 @@
 #include "SchedulerHPF.h"
-//#include <iostream>
 
 bool flaga = true;
 
 void SchedulerHPF::submitProcess(Process process)
 {
-    if(flaga)
+    if(flaga){
         switchContext();
-    flaga = false;
+        flaga = false;
+    }
     processQueue.push(process);
 }
 
 int SchedulerHPF::_currentTask()
 {
-    if(busy && currentProcess.remainingTime != 0)
-        return currentProcess.id;
-    if(processQueue.empty())
-        return 0;
+    if(busy) return currentProcess.id;
+    if(processQueue.empty()) {flaga = true; return 0;}
     return processQueue.top().id;
 }
 
@@ -24,22 +22,22 @@ void SchedulerHPF::beginStep()
 {
 	if (busy) {
         currentProcess.remainingTime--;
-		if (currentProcess.remainingTime == 0) {
-            busy = false;
-            switchContext();
-			//std::cout << "Task finished, priority: " << currentProcess.priority << " " << std::endl;
-			//std::cout << "Id = " << completedTask.id << " begin: " << completedTask.begin << " end: " << completedTask.end << std::endl;
-		}
 	}
     else {
-		if (processQueue.empty()) return;
+		if (processQueue.empty()) {
+            flaga = true;
+            return;
+        }
 
 		currentProcess = processQueue.top();
 		processQueue.pop();
         busy = true;
 
-		//std::cout << "New task #: " << currentProcess.id << " priority: " << currentProcess.priority << " at: " << currentSlice() << std::endl;
-
 		currentProcess.remainingTime--;
+	}
+
+    if (currentProcess.remainingTime == 0) {
+        busy = false;
+        switchContext();
 	}
 }
